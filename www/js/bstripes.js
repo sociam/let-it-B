94ea9d9223def5aa732e1b2d7fcc6b44a31a4716
electronic Max <electronic@gmail.com>
2014-10-06 15:56:43 +0100

    now loads at least

--- www/js/bstripes.js
+++ www/js/bstripes.js
@@ -1,13 +1,21 @@
 /* jshint undef: true, strict:false, trailing:false, unused:false */
-/* global require, exports, console, process, module, L, angular, _, jQuery, FastClick, device, document, window, setTimeout */
+/* global require, exports, console, process, module, L, angular, _, jQuery, FastClick, device, document, window, setTimeout, d3 */
 
 angular
 	.module('bstripes', ['ngAnimate']) 
-	.factor('loader', function() { 
+	.factory('loader', function(utils) { 
 		return {
-			load:function(fnames) {
-				var d = $.Deferred();
-				
+			load:function(files) {
+				var u = utils;
+				return u.when(files.map(function(f) { 
+					var d = u.deferred();
+					console.log('loading ', f);
+					d3.tsv(f, function(rows) {
+						console.log('rows ', rows);
+						d.resolve(rows);
+					});
+					return d.promise();
+				}));
 			}
 		};
 	}).controller('main', function($scope, loader) { 
@@ -15,5 +23,9 @@ angular
 			'data/snapshot_serengeti_anon_comments_DO_NOT_PUBLISH-06-10-2014.tsv',
 			'data/galaxy_zoo_anon_comments_DO_NOT_PUBLISH-06-10-2014.tsv'
 		];
-		loader.load(files).then(function() { });
+		loader.load(files).then(function(rows) { 
+			console.log('continuation ', rows);
+		}).fail(function(x) { 
+			console.error('error', x);
+		});
 	});
