649a8bbab4fda365038c94b732dbcd006ac128da
electronic Max <electronic@gmail.com>
2014-10-07 10:52:15 +0100

    bars

--- www/js/contributor-stripes.js
+++ www/js/contributor-stripes.js
@@ -12,30 +12,31 @@ angular
 				// $scope.data will be the raw data
 				// console.log('stripes scope ', $scope);
 				var u = utils,
-					sa = function(f) { utils.safeApply($scope, f); };
-
-
-				$scope.$watch('data', function(val) { 
-					if (val) { 
-						var creature = u.dictCat($scope.data.map(function(r) {
+					sa = function(f) { utils.safeApply($scope, f); }, 
+					colorscale = d3.scale.ordinal(),
+					keyOrder, byKey,
+					update = function(val) { 
+						if (val === undefined) { return; }
+						var byKey = u.dictCat($scope.data.map(function(r) {
 							return [r[$scope.field], r];
-						}));
-						var total = _(creature).values()
+						}));						
+						var total = _(byKey).values()
 									.map(function(entries) { return entries.length; })
-									.reduce(function(a,b) { return a + b; },0);
+									.reduce(function(a,b) { return a + b; }, 0);
 						if ($scope.bars === undefined) { 
 							$scope.bars = [];
-							var barnames = _(creature).keys();
+							var barnames = _(byKey).keys();
 							barnames.sort();
 							barnames.map(function(barname) {
 								$scope.bars.push({name:barname});
 							});
 						}
 						$scope.bars.map(function(b) {
-							b.pct = creature[b.name] ? creature[b.name].length/total : 0;
+							b.pct = byKey[b.name] ? byKey[b.name].length/total : 0;
 						});
-					}
-				});
+					};
+				$scope.$watch('data', update);
+				update($scope.data);
 				window.$ss = $scope;
 			}
 		};
